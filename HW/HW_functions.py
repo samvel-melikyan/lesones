9251ab9433be6f9910d7eddc2fb2715697fb839b
samvelMelikyan <122483834+samvelMelikyan@users.noreply.github.com>
2025-01-27 17:00:59 +0400

    HW2

--- HW/HW_functions.py
+++ HW/HW_functions.py
@@ -27,25 +27,35 @@ for i, answer in enumerate(answers):
 new_line("N-2")
 
 
-
 def sales_stats(list, **kwargs):
     total_revenue = None
     product_quantities = None
+    producs = {}
+
+    for item in list:
+        product_name, quantity, price = item
+        if product_name in producs.keys():
+            producs[product_name]["quantity"] += quantity
+            producs[product_name]["price"] += price
+            producs[product_name]["total_price"] += quantity * price
+        else:
+            producs[product_name] = {"quantity": quantity,
+                                     "price": price,
+                                     "total_price": quantity * price}
+    # print(producs)
+
     if kwargs.get("revenue") and kwargs.get("quantity"):
         total_revenue = sum(item[1] * item[2] for item in list)
+        product_quantities = {}
+        for i in producs:
+            product_quantities[i] = producs[i]["quantity"]
     elif kwargs.get("quantity"):
         product_quantities = {}
-        for item in list:
-            product_name, quantity, _ = item
-            if product_name in product_quantities:
-                product_quantities[product_name] += quantity
-            else:
-                product_quantities[product_name] = quantity
-        if kwargs
+        for i in producs:
+            product_quantities[i] = producs[i]["quantity"]
+    elif kwargs.get("revenue"):
+        total_revenue = sum(x["total_price"] for x in producs.values())
 
-
-
-        print(product_quantities)
     return total_revenue, product_quantities
 
 
