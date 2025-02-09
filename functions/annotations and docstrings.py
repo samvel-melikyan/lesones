7b0af3d3056abb61a780129ff8dd824dbe90f951
samvelMelikyan <samvel.melikyan.eng@gmail.com>
2025-02-09 14:52:28 +0400

    annotations

---
+++ functions/annotations and docstrings.py
@@ -0,0 +1,126 @@
+from functions import *
+new_section("annotations and docstrings")
+print("def is_valid_email(email: str):")
+print("""Если с визуальной чистотой мы начали работать — добавили аннотацию параметру email, 
+то техническая требует доработки, например, чтобы не рушить программу, мы будем возвращать None, 
+если не получили ожидаемого типа:""")
+print("""def is_valid_email(email: str):
+    if type(email) != str:
+        return None
+    
+    if email.count("@") != 1:
+        return False
+    local_part, domain_part = email.split("@")
+    if not local_part or not domain_part:
+        return False
+    if domain_part.count(".") != 1:
+        return False
+
+    return True""")
+print("def имя_функции(параметр1: ссылка_на_тип, параметр2: cсылка_на_тип, ...):")
+
+print("""азобравшись с базовой аннотацией параметров, можно задаться очевидным вопросом — 
+наверное можно как-то аннотировать тогда и тип возвращаемого значения?
+
+Ответ — да, можно, для этого давайте вернёмся к той же функции для валидации почты. 
+Тип возвращаемого значения в ней еще более неочевиден.
+def имя_функции(параметр: ссылка_на_тип) -> ссылка_на_тип:
+    ... 
+-----------------------------------------
+def is_valid_email(email: str) -> bool:
+    if email.count("@") != 1:
+        return False
+    local_part, domain_part = email.split("@")
+    if not local_part or not domain_part:
+        return False
+    if domain_part.count(".") != 1:
+        return False
+
+    return True
+
+multiple annotations for changeable variable type
+
+def calculate_power(base: int | float, exponent: int | float) -> int | float:
+    return base ** exponent
+
+""")
+print(""" Using typing ---------------
+
+from typing import Union
+
+def calculate_power(base: Union[int, float], exponent: Union[int, float]) -> Union[int, float]:
+    return base ** exponent
+
+from typing import Union
+
+def weighted_average(x: list[Union[int, float]], w: list[Union[int, float]]) -> float:
+    return sum(a*b for a,b in zip(x, w))/sum(w)
+    
+> in Python 3.9
+from typing import List, Union
+
+def weighted_average(x: List[Union[int, float]], w: List[Union[int, float]]) -> float:
+    return sum(a * b for a, b in zip(x, w)) / sum(w)
+
+Для возвращаемого значения все делается точно так же, как и для параметра:
+def count_letters(words: dict[str, int]) -> dict[str, int]:
+	letter_count = {}
+	for word in words:
+    	for letter in word:
+        	if letter in letter_count:
+            	letter_count[letter] += 1
+        	else:
+            	letter_count[letter] = 1
+	return letter_count
+
+for python 3.9 and lower
+from typing import Dict
+
+def count_letters(word_list: Dict[str, int]) -> Dict[str, int]:
+""")
+
+new_line("Vector")
+print("""
+Vector = list[float]
+vector1 = [1.0, 2.0, 1.0, 3.0]
+
+И для чего нам это пригодится? Разумеется, для функций, которые будут работать с такими векторами, 
+вот пример функции, которая меняет направление вектора:
+
+def reflect(vector: Vector) -> Vector:
+    return [ -num for num in vector]""")
+
+new_line("Callable")
+print("""
+from typing import Callable
+
+def apply_function(f: Callable[[int], str], x: int) -> str:
+    return f(x)
+Запись f: Callable[[int], str] говорит о том, что функция, которая передается в параметр f должна принимать 
+один аргумент типа int, а возвращаемое значение должно быть типа str.
+
+""")
+
+
+
+
+
+
+new_line("iterable")
+print("""Если наш объект итерируемый (например список или кортеж), то мы можем это также указать в аннотации. Это, очевидно, 
+необходимо делать в случае, если по этому объекту в теле функции будут проходиться циклом.
+
+Указать это можно при помощи того же модуля typing:
+
+from typing import Iterable
+
+def get_first_element(items: Iterable[int]) -> int:
+    return next(iter(items))""")
+
+
+
+
+
+
+
+
